7c1ace25cc2666d6c336c2d750c501836e9d4e3f
Thomas Eusebio Vieira <thomas.eusebio.vieira@egmail.com>
2021-06-19 22:30:15 -0300

    feat: improve types and remove unused file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
 import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return <Component {...pageProps} />;
 }
 export default MyApp;
